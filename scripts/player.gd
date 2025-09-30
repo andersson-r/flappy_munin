7519d0d887f7759a76e13efdb4d018a95b39fa2e
Robin Andersson <robhinio96@hotmail.com>
2025-09-30 13:54:13 +0200

    trying to add a game-over screen

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,24 +7,30 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _process(delta: float) -> void:
-	if(global_position.y > 1400): #player fall down
+	if global_position.y > 1400: #player fall down
 		_player_death()
+
+
 
 func _physics_process(delta: float) -> void:
 	#gravity
-	if(not is_on_floor()):
+	if not is_on_floor():
 		velocity.y += 20 #adjusted fall speed
 
 	#fly
-	if (Input.is_action_pressed("jump")):
+	if Input.is_action_pressed("jump"):
 		_animated_sprite.play()
 		velocity.y = JUMP_SPEED
 
 	#the player should constantly move forward
 	velocity.x = 2 * SPEED
 
-	move_and_slide()
+	var collision = move_and_collide(velocity*delta) #move and check for collisions
+	if collision:
+		_player_death()
+		
 
 
 func _player_death():
-	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+	get_tree().paused = true
+	#get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
